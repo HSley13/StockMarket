1880908a61424d2cfd95970ffdfe4ac3eea4fda3
Your Name <you@example.com>
2025-01-17 17:07:42 +0800

    Starting with the MenuPanel

--- frontend/Debug/CMakeFiles/app.dir/Debug/.qt/rcc/qrc_app_raw_qml_0.cpp.o.d
+++
@@ -1,2 +0,0 @@
-CMakeFiles/app.dir/Debug/.qt/rcc/qrc_app_raw_qml_0.cpp.o: \
-  /Users/hsley13/Dev/QML_GO_project/frontend/Debug/.qt/rcc/qrc_app_raw_qml_0.cpp
--- frontend/Debug/CMakeFiles/app.dir/Debug/.qt/rcc/qrc_qmake_stockMarket.cpp.o.d
+++
@@ -1,2 +0,0 @@
-CMakeFiles/app.dir/Debug/.qt/rcc/qrc_qmake_stockMarket.cpp.o: \
-  /Users/hsley13/Dev/QML_GO_project/frontend/Debug/.qt/rcc/qrc_qmake_stockMarket.cpp
